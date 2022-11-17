a7960a96efbb54d8bfe8c657a1d3c364b8642db2
Doomimic <doomimic@gmail.com>
2022-11-17 23:06:55 +0800

    Switched class so players keep track of damage

--- LifeTracker.cs
+++ LifeTracker.cs
@@ -4,54 +4,39 @@ using Terraria.ModLoader;
 
 namespace Leaderboards
 {
-    public class LifeTracker : GlobalNPC
+    public class LifeTracker : ModPlayer
     {
         private bool debug = true;
-        public int oldLife;
-        public int totalLifeLost = 0;
 
-        public override bool InstancePerEntity => true;
-
-        public override bool PreAI(NPC npc)
+        public void OnHitNPCWithAnything(NPC target, int damage, float knockback, bool crit, Item item = default, Projectile proj = default)
         {
-            oldLife = npc.life;
-            return true;
-        }
-
-        public void OnHitByAnything(NPC npc, int damage, float knockback, bool crit, Player player = default, Item item = default, Projectile projectile = default)
-        {
-            int lifeLost = oldLife - npc.life;
-            totalLifeLost += lifeLost;
-
             if (debug) {
-                if (player != default) {
+                if (item != default) {
                     Leaderboards.NewMessage(
-                        npc.FullName + " was hit by " + player.name + " with " + item.Name, Color.Orange
+                        target.FullName + " was hit by " + Player.name + " with " + item.Name, Color.Orange
                     );
                 }
 
-                if (projectile != default) {
+                if (proj != default) {
                     Leaderboards.NewMessage(
-                        npc.FullName + " was hit by " + Main.player[projectile.owner].name + " with " + projectile.Name, Color.Orange
+                        target.FullName + " was hit by " + Main.player[proj.owner].name + " with " + proj.Name, Color.Orange
                     );
                 }
 
                 Leaderboards.NewMessage("  Damage: " + damage);
                 Leaderboards.NewMessage("  Knockback: " + knockback);
                 Leaderboards.NewMessage("  Crit: " + crit);
-                Leaderboards.NewMessage("  Life lost: " + lifeLost);
-                Leaderboards.NewMessage("  Total life lost: " + totalLifeLost);
 
-                if (npc.life <= 0) {
-                    Leaderboards.NewMessage(npc.FullName + " was killed!", Color.Purple);
+                if (target.life <= 0) {
+                    Leaderboards.NewMessage(target.FullName + " was killed!", Color.Purple);
                 }
             }
         }
 
-        public override void OnHitByItem(NPC npc, Player player, Item item, int damage, float knockback, bool crit)
-            => OnHitByAnything(npc, damage, knockback, crit, player:player, item:item);
+        public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
+            => OnHitNPCWithAnything(target, damage, knockback, crit, item: item);
 
-        public override void OnHitByProjectile(NPC npc, Projectile projectile, int damage, float knockback, bool crit)
-            => OnHitByAnything(npc, damage, knockback, crit, projectile:projectile);
+        public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
+            => OnHitNPCWithAnything(target, damage, knockback, crit, proj: proj);
     }
 }
