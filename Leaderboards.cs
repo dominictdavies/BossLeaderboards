34545c3aaa30bd4fc12be2acb1ab16519f1ef0ab
Doomimic <doomimic@gmail.com>
2022-11-19 01:48:34 +0800

    Altered structuring and now working with only player

--- Leaderboards.cs
+++ Leaderboards.cs
@@ -1,23 +1,9 @@
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.Chat;
-using Terraria.ID;
-using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace Leaderboards
 {
     public class Leaderboards : Mod
     {
-        public static void NewMessage(string text, Color color = default)
-        {
-            if (color == default) color = Color.White;
-
-            if (Main.netMode == NetmodeID.SinglePlayer) {
-                Main.NewText(text, color);
-            } else {
-                ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(text), color);
-            }
-        }
+        public static bool debug = true;
     }
 }
--- LeaderboardsPlayer.cs
+++ LeaderboardsPlayer.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -7,37 +6,49 @@ namespace Leaderboards
 {
     public class LeaderboardsPlayer : ModPlayer
     {
-        private bool debug = true;
-
-        public Dictionary<int, int> BossDamages = new Dictionary<int, int>();
+        public int[] contributions = new int[Main.npc.Length];
 
         public void OnHitNPCWithAnything(NPC target, int damage, float knockback, bool crit, Item item = default, Projectile proj = default)
         {
-            if (!BossDamages.ContainsKey(target.whoAmI)) {
-                BossDamages.Add(target.whoAmI, damage);
-            } else {
-                BossDamages[target.whoAmI] += damage;
-            }
+            contributions[target.whoAmI] += damage;
 
-            if (debug) {
-                if (item != default) {
-                    Leaderboards.NewMessage(
-                        target.FullName + " was hit by " + Player.name + " with " + item.Name, Color.Orange
+            if (Leaderboards.debug) {
+                if (item != default) Main.NewText(
+                        target.FullName + " was hit by " + Player.name + " with " + item.Name,
+                        Color.Orange
                     );
-                }
 
-                if (proj != default) {
-                    Leaderboards.NewMessage(
-                        target.FullName + " was hit by " + Main.player[proj.owner].name + " with " + proj.Name, Color.Orange
+                if (proj != default) Main.NewText(
+                        target.FullName + " was hit by " + Main.player[proj.owner].name + " with " + proj.Name,
+                        Color.Orange
                     );
-                }
 
-                Leaderboards.NewMessage("  Damage: " + damage);
-                Leaderboards.NewMessage("  Knockback: " + knockback);
-                Leaderboards.NewMessage("  Crit: " + crit);
+                //Main.NewText("  Damage: " + damage);
+                //Main.NewText("  Knockback: " + knockback);
+                //Main.NewText("  Crit: " + crit);
+
+                for (int i = 0; i < Main.npc.Length; i++) {
+                    if (contributions[i] != 0) {
+                        Main.NewText("contributions[" + i + "] = " + contributions[i]);
+                    }
+                }
 
                 if (target.life <= 0) {
-                    Leaderboards.NewMessage(target.FullName + " was killed!", Color.Purple);
+                    Main.NewText(target.FullName + " was killed!", Color.Purple);
+                    contributions[target.whoAmI] = 0;
+
+                    //foreach (Player player in Main.player) {
+                    //    if (player.active) {
+                    //        int[] contributions = player.GetModPlayer<LeaderboardsPlayer>().contributions;
+                    //        if (contributions[npc.whoAmI] != 0) {
+                    //            Main.NewText(
+                    //                player.name + " dealt " + contributions[npc.whoAmI].ToString() + " damage to " + npc.FullName,
+                    //                Color.Yellow
+                    //            );
+                    //            contributions[npc.whoAmI] = 0;
+                    //        }
+                    //    }
+                    //}
                 }
             }
         }
@@ -46,6 +57,6 @@ namespace Leaderboards
             => OnHitNPCWithAnything(target, damage, knockback, crit, item: item);
 
         public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
-            => OnHitNPCWithAnything(target, damage, knockback, crit, proj: proj);
+            => OnHitNPCWithAnything(target, crit ? damage * 2 : damage, knockback, crit, proj: proj);
     }
 }
