8f11b7369c8e0392dbcf393f4ab07843fbc59dac
doomimic <doomimic@gmail.com>
2022-12-09 01:43:14 +0800

    Added contribution as a class

---
+++ Contribution.cs
@@ -0,0 +1,16 @@
+﻿namespace Leaderboards
+{
+    public class Contribution
+    {
+        public int bossWhoAmI;
+        public int totalDamage;
+        public int totalLifeLost;
+
+        public Contribution(int bossWhoAmI, int totalDamage = 0, int totalLifeLost = 0)
+        {
+            this.bossWhoAmI = bossWhoAmI;
+            this.totalDamage = totalDamage;
+            this.totalLifeLost = totalLifeLost;
+        }
+    }
+}
