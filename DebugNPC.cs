4b50688d302ad76a335df82fdcdacbe841846bd9
doomimic <doomimic@gmail.com>
2023-06-03 20:47:18 +0800

    DebugNPC removed from gitignore

---
+++ DebugNPC.cs
@@ -0,0 +1,16 @@
+﻿// Debug file
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Leaderboards
+{
+    public class DebugNPC : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            if (npc.type == NPCID.SkeletronHead) npc.aiStyle = 0;
+        }
+    }
+}
