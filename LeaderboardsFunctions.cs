0943a94092ca3ce5a1499de720692ed123665112
doomimic <doomimic@gmail.com>
2023-06-04 00:24:39 +0800

    Contribution push if > 0

--- LeaderboardsFunctions.cs
+++ LeaderboardsFunctions.cs
@@ -6,22 +6,24 @@ namespace Leaderboards
 {
     public static class LeaderboardsFunctions
     {
-        public const int debug = 1;
+        public const int debug = 0;
 
         public static void PushContribution(Player player)
         {
             LeaderboardsPlayer leaderboardsPlayer = player.GetModPlayer<LeaderboardsPlayer>();
 
             foreach (KeyValuePair<string, Contribution> bossContribution in leaderboardsPlayer.contributions) {
-                Main.NewText(
-                    player.name + " dealt " + bossContribution.Value.totalDamageTo + " damage to " + bossContribution.Key + ".",
-                    Color.Magenta
-                );
+                if (bossContribution.Value.totalDamageTo > 0 || debug > 0)
+                    Main.NewText(
+                        player.name + " dealt " + bossContribution.Value.totalDamageTo + " damage to " + bossContribution.Key + ".",
+                        Color.Magenta
+                    );
 
-                Main.NewText(
-                    player.name + " lost " + bossContribution.Value.totalLifeLostFrom + " life to " + bossContribution.Key + ".",
-                    Color.Red
-                );
+                if (bossContribution.Value.totalLifeLostFrom > 0 || debug > 0)
+                    Main.NewText(
+                        player.name + " lost " + bossContribution.Value.totalLifeLostFrom + " life to " + bossContribution.Key + ".",
+                        Color.Red
+                    );
 
                 leaderboardsPlayer.contributions.Remove(bossContribution.Key);
             }
