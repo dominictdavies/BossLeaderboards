75a11f1f422e455352929a63ef7a052403a53f5d
doomimic <doomimic@gmail.com>
2023-06-10 22:26:53 +0800

    Text showing up hard coded

--- UI/Leaderboard.cs
+++ UI/Leaderboard.cs
@@ -59,17 +59,53 @@ namespace Leaderboards.UI
         private void OnResetButtonClick(UIMouseEvent evt, UIElement listeningElement)
         {
             leaderPanel.RemoveAllChildren();
+
+            UIList nameList = new UIList();
+            nameList.Width.Set(280 / 3, 0);
+            nameList.Height.Set(220, 0);
+            nameList.HAlign = 0f;
+            leaderPanel.Append(nameList);
+            UIText headingName = new UIText("Name");
+            headingName.HAlign = 0.5f;
+            nameList.Add(headingName);
+
+            UIList damageList = new UIList();
+            damageList.Width.Set(280 / 3, 0);
+            damageList.Height.Set(220, 0);
+            damageList.HAlign = 0.5f;
+            leaderPanel.Append(damageList);
+            UIText headingDamage = new UIText("Damage");
+            headingDamage.HAlign = 0.5f;
+            damageList.Add(headingDamage);
+
+            UIList lifeLostList = new UIList();
+            lifeLostList.Width.Set(280 / 3, 0);
+            lifeLostList.Height.Set(220, 0);
+            lifeLostList.HAlign = 1f;
+            leaderPanel.Append(lifeLostList);
+            UIText headingLifeLost = new UIText("Life Lost");
+            headingLifeLost.HAlign = 0.5f;
+            lifeLostList.Add(headingLifeLost);
+
             for (int i = 0; i < Main.player.Length; i++)
             {
                 Player player = Main.player[i];
                 if (player.active)
                 {
-                    UIText playerText = new UIText(player.name);
-                    playerText.HAlign = 0.5f;
-                    playerText.Top.Set(i * 15, 0);
-                    leaderPanel.Append(playerText);
+                    UIText name = new UIText(player.name);
+                    name.HAlign = 0.5f;
+                    nameList.Add(name);
+
+                    UIText damage = new UIText(player.statDefense.ToString());
+                    damage.HAlign = 0.5f;
+                    damageList.Add(damage);
+
+                    UIText lifeLost = new UIText(player.statLife.ToString());
+                    lifeLost.HAlign = 0.5f;
+                    lifeLostList.Add(lifeLost);
                 }
             }
+
             SoundEngine.PlaySound(SoundID.Chat);
         }
 
