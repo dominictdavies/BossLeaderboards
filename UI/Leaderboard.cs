85d7a25b07e3e3963bd79cf958d93abce4e8d749
doomimic <doomimic@gmail.com>
2023-06-10 19:46:43 +0800

    Leaderboard showing players

--- UI/Leaderboard.cs
+++ UI/Leaderboard.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ModLoader;
 using Terraria.UI;
 using Terraria.GameContent.UI.Elements;
 using Terraria.Audio;
@@ -8,26 +9,28 @@ namespace Leaderboards.UI
 {
     public class Leaderboard : UIState
     {
+        private UIPanel leaderPanel;
+
         public override void OnInitialize()
         {
-            UIPanel panel = new UIPanel();
-            panel.Width.Set(300, 0);
-            panel.Height.Set(300, 0);
-            panel.HAlign = panel.VAlign = 0.5f;
-            Append(panel);
+            UIPanel masterPanel = new UIPanel();
+            masterPanel.Width.Set(300, 0);
+            masterPanel.Height.Set(300, 0);
+            masterPanel.HAlign = masterPanel.VAlign = 0.5f;
+            Append(masterPanel);
 
             UIText title = new UIText("Leaderboard");
             title.HAlign = 0.5f;
             title.Top.Set(15, 0);
-            panel.Append(title);
+            masterPanel.Append(title);
 
             UIPanel resetButton = new UIPanel();
             resetButton.Width.Set(30, 0);
             resetButton.Height.Set(30, 0);
             resetButton.Top.Set(10, 0);
             resetButton.Left.Set(10, 0);
-            resetButton.OnClick += OnCloseButtonClick;
-            panel.Append(resetButton);
+            resetButton.OnClick += OnResetButtonClick;
+            masterPanel.Append(resetButton);
 
             UIText resetText = new UIText("V");
             resetText.HAlign = resetText.VAlign = 0.5f;
@@ -39,17 +42,41 @@ namespace Leaderboards.UI
             closeButton.Top.Set(10, 0);
             closeButton.Left.Set(-10 - closeButton.Width.Pixels, 1);
             closeButton.OnClick += OnCloseButtonClick;
-            panel.Append(closeButton);
+            masterPanel.Append(closeButton);
 
             UIText closeText = new UIText("X");
             closeText.HAlign = closeText.VAlign = 0.5f;
             closeButton.Append(closeText);
+
+            leaderPanel = new UIPanel();
+            leaderPanel.Width.Set(280, 0);
+            leaderPanel.Height.Set(220, 0);
+            leaderPanel.Top.Set(55, 0);
+            leaderPanel.HAlign = 0.5f;
+            masterPanel.Append(leaderPanel);
+        }
+
+        private void OnResetButtonClick(UIMouseEvent evt, UIElement listeningElement)
+        {
+            leaderPanel.RemoveAllChildren();
+            for (int i = 0; i < Main.player.Length; i++)
+            {
+                Player player = Main.player[i];
+                if (player.active)
+                {
+                    UIText playerText = new UIText(player.name);
+                    playerText.HAlign = 0.5f;
+                    playerText.Top.Set(i * 15, 0);
+                    leaderPanel.Append(playerText);
+                }
+            }
+            SoundEngine.PlaySound(SoundID.Chat);
         }
 
         private void OnCloseButtonClick(UIMouseEvent evt, UIElement listeningElement)
         {
             ModContent.GetInstance<LeaderboardSystem>().HideMyUI();
-            SoundEngine.PlaySound(SoundID.MenuTick);
+            SoundEngine.PlaySound(SoundID.MenuClose);
         }
     }
 }
