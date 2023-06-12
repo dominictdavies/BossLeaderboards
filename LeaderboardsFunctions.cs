84340c39f8c7679bd99d86dc3a0a1dfab2dec2f8
doomimic <doomimic@gmail.com>
2023-06-13 01:29:31 +0800

    Bug fix

--- LeaderboardsFunctions.cs
+++ LeaderboardsFunctions.cs
@@ -12,14 +12,14 @@ namespace Leaderboards
             LeaderboardsPlayer leaderboardsPlayer = player.GetModPlayer<LeaderboardsPlayer>();
             Contribution contribution = leaderboardsPlayer.contribution;
 
-            if (contribution.damage > 0 || debug > 0)
-                Main.NewText(
+            if (debug > 0)
+                Main.NewTextMultiline(
                     player.name + " dealt " + contribution.damage + " damage.\n" +
                     player.name + " killed " + contribution.kills + " enemies.\n" +
                     player.name + " lost " + contribution.lifeLost + " life.\n" +
                     player.name + " took " + contribution.hitsTaken + " hits.\n" +
-                    player.name + " died " + contribution.deaths + " times.\n",
-                    Color.Magenta
+                    player.name + " died " + contribution.deaths + " times.",
+                    c: Color.Magenta
                 );
 
             contribution.Reset();
