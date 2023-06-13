dd5961db2d5c606f960c332d5cc20235ad0158af
dominictdavies <dominictdavies@gmail.com>
2023-06-13 14:43:35 +0800

    Refactoring

--- UI/Leaderboard.cs
+++ UI/Leaderboard.cs
@@ -1,5 +1,4 @@
-﻿using Terraria;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 using Terraria.UI;
 using Terraria.GameContent.UI.Elements;
 using Terraria.Audio;
@@ -9,34 +8,38 @@ namespace Leaderboards.UI
 {
     public class Leaderboard : UIState
     {
+        private UIPanel masterPanel;
+        private UIText title;
+        private UIPanel closeButton;
+        private UIText closeText;
         private UIPanel leaderPanel;
 
         public override void OnInitialize()
         {
-            UIPanel masterPanel = new UIPanel();
+            masterPanel = new UIPanel();
             masterPanel.Width.Set(300, 0);
             masterPanel.Height.Set(300, 0);
             masterPanel.HAlign = masterPanel.VAlign = 0.5f;
             Append(masterPanel);
 
-            UIText title = new UIText("Leaderboard");
+            title = new UIText("Leaderboard");
             title.HAlign = 0.5f;
             title.Top.Set(15, 0);
             masterPanel.Append(title);
 
-            UIPanel resetButton = new UIPanel();
-            resetButton.Width.Set(30, 0);
-            resetButton.Height.Set(30, 0);
-            resetButton.Top.Set(10, 0);
-            resetButton.Left.Set(10, 0);
-            resetButton.OnClick += OnResetButtonClick;
-            masterPanel.Append(resetButton);
+            // UIPanel resetButton = new UIPanel();
+            // resetButton.Width.Set(30, 0);
+            // resetButton.Height.Set(30, 0);
+            // resetButton.Top.Set(10, 0);
+            // resetButton.Left.Set(10, 0);
+            // resetButton.OnClick += OnResetButtonClick;
+            // masterPanel.Append(resetButton);
 
-            UIText resetText = new UIText("V");
-            resetText.HAlign = resetText.VAlign = 0.5f;
-            resetButton.Append(resetText);
+            // UIText resetText = new UIText("V");
+            // resetText.HAlign = resetText.VAlign = 0.5f;
+            // resetButton.Append(resetText);
 
-            UIPanel closeButton = new UIPanel();
+            closeButton = new UIPanel();
             closeButton.Width.Set(30, 0);
             closeButton.Height.Set(30, 0);
             closeButton.Top.Set(10, 0);
@@ -44,7 +47,7 @@ namespace Leaderboards.UI
             closeButton.OnClick += OnCloseButtonClick;
             masterPanel.Append(closeButton);
 
-            UIText closeText = new UIText("X");
+            closeText = new UIText("X");
             closeText.HAlign = closeText.VAlign = 0.5f;
             closeButton.Append(closeText);
 
@@ -56,58 +59,58 @@ namespace Leaderboards.UI
             masterPanel.Append(leaderPanel);
         }
 
-        private void OnResetButtonClick(UIMouseEvent evt, UIElement listeningElement)
-        {
-            leaderPanel.RemoveAllChildren();
+        // private void OnResetButtonClick(UIMouseEvent evt, UIElement listeningElement)
+        // {
+        //     leaderPanel.RemoveAllChildren();
 
-            UIList nameList = new UIList();
-            nameList.Width.Set(280 / 3, 0);
-            nameList.Height.Set(220, 0);
-            nameList.HAlign = 0f;
-            leaderPanel.Append(nameList);
-            UIText headingName = new UIText("Name");
-            headingName.HAlign = 0.5f;
-            nameList.Add(headingName);
+        //     UIList nameList = new UIList();
+        //     nameList.Width.Set(280 / 3, 0);
+        //     nameList.Height.Set(220, 0);
+        //     nameList.HAlign = 0f;
+        //     leaderPanel.Append(nameList);
+        //     UIText headingName = new UIText("Name");
+        //     headingName.HAlign = 0.5f;
+        //     nameList.Add(headingName);
 
-            UIList damageList = new UIList();
-            damageList.Width.Set(280 / 3, 0);
-            damageList.Height.Set(220, 0);
-            damageList.HAlign = 0.5f;
-            leaderPanel.Append(damageList);
-            UIText headingDamage = new UIText("Damage");
-            headingDamage.HAlign = 0.5f;
-            damageList.Add(headingDamage);
+        //     UIList damageList = new UIList();
+        //     damageList.Width.Set(280 / 3, 0);
+        //     damageList.Height.Set(220, 0);
+        //     damageList.HAlign = 0.5f;
+        //     leaderPanel.Append(damageList);
+        //     UIText headingDamage = new UIText("Damage");
+        //     headingDamage.HAlign = 0.5f;
+        //     damageList.Add(headingDamage);
 
-            UIList lifeLostList = new UIList();
-            lifeLostList.Width.Set(280 / 3, 0);
-            lifeLostList.Height.Set(220, 0);
-            lifeLostList.HAlign = 1f;
-            leaderPanel.Append(lifeLostList);
-            UIText headingLifeLost = new UIText("Life Lost");
-            headingLifeLost.HAlign = 0.5f;
-            lifeLostList.Add(headingLifeLost);
+        //     UIList lifeLostList = new UIList();
+        //     lifeLostList.Width.Set(280 / 3, 0);
+        //     lifeLostList.Height.Set(220, 0);
+        //     lifeLostList.HAlign = 1f;
+        //     leaderPanel.Append(lifeLostList);
+        //     UIText headingLifeLost = new UIText("Life Lost");
+        //     headingLifeLost.HAlign = 0.5f;
+        //     lifeLostList.Add(headingLifeLost);
 
-            for (int i = 0; i < Main.player.Length; i++)
-            {
-                Player player = Main.player[i];
-                if (player.active)
-                {
-                    UIText name = new UIText(player.name);
-                    name.HAlign = 0.5f;
-                    nameList.Add(name);
+        //     for (int i = 0; i < Main.player.Length; i++)
+        //     {
+        //         Player player = Main.player[i];
+        //         if (player.active)
+        //         {
+        //             UIText name = new UIText(player.name);
+        //             name.HAlign = 0.5f;
+        //             nameList.Add(name);
 
-                    UIText damage = new UIText(player.statDefense.ToString());
-                    damage.HAlign = 0.5f;
-                    damageList.Add(damage);
+        //             UIText damage = new UIText(player.statDefense.ToString());
+        //             damage.HAlign = 0.5f;
+        //             damageList.Add(damage);
 
-                    UIText lifeLost = new UIText(player.statLife.ToString());
-                    lifeLost.HAlign = 0.5f;
-                    lifeLostList.Add(lifeLost);
-                }
-            }
+        //             UIText lifeLost = new UIText(player.statLife.ToString());
+        //             lifeLost.HAlign = 0.5f;
+        //             lifeLostList.Add(lifeLost);
+        //         }
+        //     }
 
-            SoundEngine.PlaySound(SoundID.Chat);
-        }
+        //     SoundEngine.PlaySound(SoundID.Chat);
+        // }
 
         private void OnCloseButtonClick(UIMouseEvent evt, UIElement listeningElement)
         {
