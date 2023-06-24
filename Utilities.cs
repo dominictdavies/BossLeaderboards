0773d06852c87fa5d7880044f4de8dd5900db6da
doomimic <doomimic@gmail.com>
2023-06-24 18:35:33 +0800

    Add utilities, GetActivePlayers() implemented

---
+++ Utilities.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Terraria;
+
+namespace Leaderboards.Utilities
+{
+    public static class Utilities
+    {
+        public static List<Player> GetActivePlayers()
+        {
+            List<Player> activePlayers = new List<Player>();
+
+            for (int i = 0; i < Main.maxPlayers; i++)
+            {
+                Player player = Main.player[i];
+                if (player.active)
+                    activePlayers.Add(player);
+            }
+
+            return activePlayers;
+        }
+    }
+}
