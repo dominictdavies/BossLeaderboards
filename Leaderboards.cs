a4e634d8e4e0145069be1fda5986c00f7831e0f4
dominictdavies <dominictdavies@gmail.com>
2023-06-27 17:07:47 +0800

    Netcode draft

--- Leaderboards.cs
+++ Leaderboards.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
@@ -13,37 +14,60 @@ namespace Leaderboards
         {
             if (Main.netMode == NetmodeID.Server)
             {
-                UpdateContribution(reader);
+                long[] values = new long[] {
+                    reader.ReadInt64(),
+                    reader.ReadInt64(),
+                    reader.ReadInt64(),
+                    reader.ReadInt64(),
+                    reader.ReadInt64()
+                };
+                UpdateContribution(Main.player[whoAmI], values);
 
-                if (++_recievedContributions >= Utilities.GetActivePlayers().Count)
+                List<Player> activePlayers = Utilities.GetActivePlayers();
+                if (++_recievedContributions >= activePlayers.Count)
                 {
-                    // Send packet to other clients containing sender and contribution
+                    // Send large packet to all clients containing state of leaderboard
                     ModPacket packet = GetPacket();
-                    packet.Write((byte)whoAmI);
-                    packet.Write(reader.ReadInt64()); // damage
-                    packet.Write(reader.ReadInt64()); // kills
-                    packet.Write(reader.ReadInt64()); // lifeLost
-                    packet.Write(reader.ReadInt64()); // hitsTaken
-                    packet.Write(reader.ReadInt64()); // deaths
+                    packet.Write((byte)activePlayers.Count);
+                    foreach (Player player in activePlayers)
+                    {
+                        Contribution contribution = player.GetModPlayer<LeaderboardsPlayer>().contribution;
+                        packet.Write((byte)player.whoAmI);
+                        packet.Write((long)contribution.GetStat("Damage"));
+                        packet.Write((long)contribution.GetStat("Kills"));
+                        packet.Write((long)contribution.GetStat("Life Lost"));
+                        packet.Write((long)contribution.GetStat("Hits Taken"));
+                        packet.Write((long)contribution.GetStat("Deaths"));
+                    }
                     packet.Send();
                     _recievedContributions = 0;
                 }
             }
             else if (Main.netMode == NetmodeID.MultiplayerClient)
             {
-
+                for (byte i = 0; i < reader.ReadByte(); i++)
+                {
+                    byte playerIndex = reader.ReadByte();
+                    long[] values = new long[] {
+                        reader.ReadInt64(),
+                        reader.ReadInt64(),
+                        reader.ReadInt64(),
+                        reader.ReadInt64(),
+                        reader.ReadInt64()
+                    };
+                    UpdateContribution(Main.player[playerIndex], values);
+                }
             }
         }
 
-        private void UpdateContribution(BinaryReader reader)
+        private void UpdateContribution(Player player, long[] values)
         {
-            Player player = Main.player[reader.ReadByte()]; // whoAmI
             Contribution contribution = player.GetModPlayer<LeaderboardsPlayer>().contribution;
-            contribution.SetStat("Damage", reader.ReadInt64());
-            contribution.SetStat("Kills", reader.ReadInt64());
-            contribution.SetStat("Life Lost", reader.ReadInt64());
-            contribution.SetStat("Hits Taken", reader.ReadInt64());
-            contribution.SetStat("Deaths", reader.ReadInt64());
+            contribution.SetStat("Damage", values[0]);
+            contribution.SetStat("Kills", values[1]);
+            contribution.SetStat("Life Lost", values[2]);
+            contribution.SetStat("Hits Taken", values[3]);
+            contribution.SetStat("Deaths", values[4]);
         }
     }
 }
