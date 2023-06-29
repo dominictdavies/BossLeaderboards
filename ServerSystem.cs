d9306550b231cd45e597cfbe4c6fbf2d595cc0e7
doomimic <doomimic@gmail.com>
2023-06-30 00:56:25 +0800

    Functional yet unneat

--- ServerSystem.cs
+++ ServerSystem.cs
@@ -8,10 +8,13 @@ namespace Leaderboards
     {
         private const int PacketTimerMax = 30;
         private int _packetTimer = PacketTimerMax;
+        private const int StallTimerMax = 10 * 60;
+        private int _stallTimer = 0;
+        private bool _doStall = false;
 
         public override void PreUpdateWorld()
         {
-            if (Main.CurrentFrameFlags.AnyActiveBossNPC && --_packetTimer == 0) // Needs fix, figure out when players have contribution for server
+            if ((Main.CurrentFrameFlags.AnyActiveBossNPC || _stallTimer > 0) && --_packetTimer == 0) // Needs fix, figure out when players have contribution for server
             {
                 // Send large packet to all clients containing state of leaderboard
                 List<Player> activePlayers = Utilities.GetActivePlayers();
@@ -30,6 +33,12 @@ namespace Leaderboards
                 packet.Send();
                 Mod.Logger.Debug("Sent large packet.");
                 _packetTimer = PacketTimerMax;
+                _doStall = true;
+            }
+            else if (_doStall)
+            {
+                _stallTimer = StallTimerMax;
+                _doStall = false;
             }
         }
     }
