cc13fb30369f1a36bd5126a7e427e16f408fa983
doomimic <doomimic@gmail.com>
2023-06-30 16:46:23 +0800

    Separate UI system files

---
+++ UI/LeaderboardInterfaceSystem.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace Leaderboards.UI
+{
+    internal partial class LeaderboardSystem : ModSystem
+    {
+        internal UserInterface leaderboardInterface;
+        internal UILeaderboard leaderboard;
+        private GameTime _lastUpdateUiGameTime;
+
+        public override void Load()
+        {
+            if (Main.dedServ)
+                return;
+
+            leaderboardInterface = new UserInterface();
+            leaderboard = new UILeaderboard();
+            leaderboard.Activate();
+        }
+
+        public override void Unload()
+        {
+            leaderboard = null;
+        }
+
+        internal void ShowMyUI()
+        {
+            leaderboardInterface?.SetState(leaderboard);
+        }
+
+        internal void HideMyUI()
+        {
+            leaderboardInterface?.SetState(null);
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+            if (mouseTextIndex == -1)
+                return;
+
+            layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+                "Leaderboards: LeaderboardInterface",
+                delegate
+                {
+                    if (_lastUpdateUiGameTime != null && leaderboardInterface?.CurrentState != null)
+                        leaderboardInterface.Draw(Main.spriteBatch, _lastUpdateUiGameTime);
+                    return true;
+                },
+                InterfaceScaleType.UI
+                )
+            );
+        }
+    }
+}
--- UI/LeaderboardSystem.cs
+++ UI/LeaderboardSystem.cs
@@ -1,46 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
-using Terraria.UI;
 using Terraria.ID;
 
 namespace Leaderboards.UI
 {
-    internal class LeaderboardSystem : ModSystem
+    internal partial class LeaderboardSystem : ModSystem
     {
-        internal UserInterface leaderboardInterface;
-        internal UILeaderboard leaderboard;
-
-        public override void Load()
-        {
-            if (Main.dedServ)
-                return;
-
-            leaderboardInterface = new UserInterface();
-            leaderboard = new UILeaderboard();
-            leaderboard.Activate();
-        }
-
-        public override void Unload()
-        {
-            leaderboard = null;
-        }
-
-        internal void ShowMyUI()
-        {
-            leaderboardInterface?.SetState(leaderboard);
-        }
-
-        internal void HideMyUI()
-        {
-            leaderboardInterface?.SetState(null);
-        }
-
         private const int PacketTimerMax = 15;
         private int _packetTimer = PacketTimerMax;
         private bool _oldAnyActiveBossNPC = false;
-        private GameTime _lastUpdateUiGameTime;
 
         public override void UpdateUI(GameTime gameTime)
         {
@@ -89,25 +58,6 @@ namespace Leaderboards.UI
             foreach (string statName in Contribution.StatNames)
                 packet.Write((long)contribution.GetStat(statName));
             packet.Send();
-        }
-
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
-            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
-            if (mouseTextIndex == -1)
-                return;
-
-            layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
-                "Leaderboards: LeaderboardInterface",
-                delegate
-                {
-                    if (_lastUpdateUiGameTime != null && leaderboardInterface?.CurrentState != null)
-                        leaderboardInterface.Draw(Main.spriteBatch, _lastUpdateUiGameTime);
-                    return true;
-                },
-                InterfaceScaleType.UI
-                )
-            );
         }
     }
 }
