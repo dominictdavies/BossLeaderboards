d86f4ccf34d164d1e0da42d149a970f5fd7dd292
doomimic <doomimic@gmail.com>
2023-07-02 01:12:14 +0800

    Added trackedPlayers fix

--- ServerSystem.cs
+++ ServerSystem.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -11,24 +12,35 @@ namespace Leaderboards
         private const int StallTimerMax = 5 * 60;
         private int _stallTimer = 0;
         private bool _oldAnyActiveBossNPC = false;
+        private List<Player> _trackedPlayers;
 
         public override void PreUpdateWorld()
         {
-            if ((Main.CurrentFrameFlags.AnyActiveBossNPC || --_stallTimer > 0) && --_packetTimer == 0)
+            if (Main.CurrentFrameFlags.AnyActiveBossNPC || --_stallTimer > 0)
             {
-                // Send large packet to all clients containing state of leaderboard
-                List<Player> activePlayers = Utilities.GetActivePlayers();
-                ModPacket packet = Mod.GetPacket();
-                packet.Write(activePlayers.Count);
-                foreach (Player player in activePlayers)
+                if (Main.CurrentFrameFlags.AnyActiveBossNPC && !_oldAnyActiveBossNPC) // Boss just spawned 
                 {
-                    Contribution contribution = player.GetModPlayer<LeaderboardsPlayer>().contribution;
-                    packet.Write(player.whoAmI);
-                    foreach (string stat in Contribution.StatNames)
-                        packet.Write((long)contribution.GetStat(stat));
+                    _trackedPlayers = Utilities.GetActivePlayers();
+                    foreach (Player player in _trackedPlayers)
+                        player.GetModPlayer<LeaderboardsPlayer>().contribution = new Contribution();
                 }
-                packet.Send();
-                _packetTimer = PacketTimerMax;
+
+                if (--_packetTimer == 0)
+                {
+                    // Send large packet to all clients containing state of leaderboard
+                    List<Player> activeTracked = _trackedPlayers.Where(player => player.active).ToList();
+                    ModPacket packet = Mod.GetPacket();
+                    packet.Write(activeTracked.Count);
+                    foreach (Player player in activeTracked)
+                    {
+                        Contribution contribution = player.GetModPlayer<LeaderboardsPlayer>().contribution;
+                        packet.Write(player.whoAmI);
+                        foreach (string stat in Contribution.StatNames)
+                            packet.Write((long)contribution.GetStat(stat));
+                    }
+                    packet.Send();
+                    _packetTimer = PacketTimerMax;
+                }
             }
             else if (!Main.CurrentFrameFlags.AnyActiveBossNPC && _oldAnyActiveBossNPC) // Boss just died
             {
